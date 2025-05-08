d7c7a323186d6ed035ad78fee89deeaa1b95c885
isakidev <gaspar.c.developer@gmail.com>
2025-05-08 19:31:35 -0400

    feat: add mocks to tests

---
+++ test/mock/file-information.mock.js
@@ -0,0 +1,1 @@
+export const { fileDestination, fileName } = { fileName: 'task.json', fileDestination: 'src/outputs' }
---
+++ test/mock/tasks.mock.js
@@ -0,0 +1,5 @@
+export const tasks = [
+  { "id": 1, "description": "Learn TS", "status": "TODO", "createdAt": "2025-05-08T03:51:08.408Z" },
+  { "id": 2, "description": "Learn VIM", "status": "TODO", "createdAt": "2025-05-08T03:51:12.129Z" },
+  { "id": 3, "description": "Learn CSS", "status": "TODO", "createdAt": "2025-05-08T03:51:14.608Z" }
+]
