d9a2aa1ab9b1d8584fef68d2dd4c0a6c22f8ea67
isakidev <gaspar.c.developer@gmail.com>
2025-05-09 00:39:48 -0400

    test: add task to the mock

--- test/mock/tasks.mock.js
+++ test/mock/tasks.mock.js
@@ -1,5 +1,6 @@
 export const tasks = [
   { "id": 1, "description": "Learn TS", "status": "TODO", "createdAt": "2025-05-08T03:51:08.408Z" },
   { "id": 2, "description": "Learn VIM", "status": "TODO", "createdAt": "2025-05-08T03:51:12.129Z" },
-  { "id": 3, "description": "Learn CSS", "status": "TODO", "createdAt": "2025-05-08T03:51:14.608Z" }
+  { "id": 3, "description": "Learn CSS", "status": "TODO", "createdAt": "2025-05-08T03:51:14.608Z" },
+  { "id": 4, "description": "Learn LINUX", "status": "IN_PROGRESS", "createdAt": "2025-05-08T03:51:14.608Z" }
 ]
